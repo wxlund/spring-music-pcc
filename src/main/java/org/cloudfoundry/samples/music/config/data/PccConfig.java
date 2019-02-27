522655307df819601b61703a984451821131cc96
Zac Hansen <zhansen@pivotal.io>
2019-02-27 12:39:30 -0700

    removed PccCloudConfig as spring-boot-gemfire does auto bind for you.
    Do not use cloud-connectors and SBG together.

--- src/main/java/org/cloudfoundry/samples/music/config/data/PccConfig.java
+++ src/main/java/org/cloudfoundry/samples/music/config/data/PccConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
-//@EnableGemFireHttpSession(poolName = "DEFAULT")
 @EnableEntityDefinedRegions(basePackages = "org.cloudfoundry.samples.music.domain")
 @EnableGemfireRepositories(basePackageClasses = org.cloudfoundry.samples.music.repositories.pcc.PccAlbumRepository.class)
-// @EnableGemfireRepositories(basePackages = "org.cloudfoundry.samples.music.repositories.pcc")
 @Configuration
 @Profile("pcc")
 public class PccConfig {
-	
+
 
 }
